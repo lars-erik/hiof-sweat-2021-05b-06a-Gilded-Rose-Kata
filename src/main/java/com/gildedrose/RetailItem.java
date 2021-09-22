0a2a02aef649c3b600719f995fefeebd8a553020
Lars-Erik <lea@markedspartner.no>
2021-09-22 13:32:00 +0200

    Extracted increaseQuality

---
+++ src/main/java/com/gildedrose/RetailItem.java
@@ -0,0 +1,55 @@
+package com.gildedrose;
+
+public class RetailItem extends Item {
+    public RetailItem(String name, int sellIn, int quality) {
+        super(name, sellIn, quality);
+    }
+
+    void updateItemQuality() {
+        if (!name.equals("Aged Brie")
+                && !name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (quality > 0) {
+                if (!name.equals("Sulfuras, Hand of Ragnaros")) {
+                    quality = quality - 1;
+                }
+            }
+        } else {
+            increaseQuality();
+
+            if (name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (sellIn < 11) {
+                    increaseQuality();
+                }
+
+                if (sellIn < 6) {
+                    increaseQuality();
+                }
+            }        }
+
+        if (!name.equals("Sulfuras, Hand of Ragnaros")) {
+            sellIn = sellIn - 1;
+        }
+
+        if (sellIn < 0) {
+            if (!name.equals("Aged Brie")) {
+                if (!name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (quality > 0) {
+                        if (!name.equals("Sulfuras, Hand of Ragnaros")) {
+                            quality = quality - 1;
+                        }
+                    }
+                } else {
+                    quality = quality - quality;
+                }
+            } else {
+                increaseQuality();
+            }
+        }
+    }
+
+    private void increaseQuality() {
+        if (quality < 50) {
+            quality = quality + 1;
+        }
+    }
+}
--- src/test/java/com/gildedrose/TexttestFixture.java
+++ src/test/java/com/gildedrose/TexttestFixture.java
@@ -34,15 +34,15 @@ public class TexttestFixture {
 
     public static Item[] createItems() {
         return new Item[] {
-                new Item("+5 Dexterity Vest", 10, 20), //
-                new Item("Aged Brie", 2, 0), //
-                new Item("Elixir of the Mongoose", 5, 7), //
-                new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
-                new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+                new RetailItem("+5 Dexterity Vest", 10, 20), //
+                new RetailItem("Aged Brie", 2, 0), //
+                new RetailItem("Elixir of the Mongoose", 5, 7), //
+                new RetailItem("Sulfuras, Hand of Ragnaros", 0, 80), //
+                new RetailItem("Sulfuras, Hand of Ragnaros", -1, 80),
+                new RetailItem("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+                new RetailItem("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+                new RetailItem("Backstage passes to a TAFKAL80ETC concert", 5, 49),
                 // this conjured item does not work properly yet
-                new Item("Conjured Mana Cake", 3, 6) };
+                new RetailItem("Conjured Mana Cake", 3, 6) };
     }
 }
